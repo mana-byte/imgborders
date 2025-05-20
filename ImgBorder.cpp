b9c92d52b953ccf59a58f42e148b60d2a5466b69
zac <zac@zacoons.com>
2025-05-20 21:39:24 +1000

    began adding borders (now requires Hyprland >= commit b90910c)

--- ImgBorder.cpp
+++ ImgBorder.cpp
@@ -9,9 +9,29 @@
 #include <hyprland/src/render/Texture.hpp>
 #include <hyprland/src/render/decorations/DecorationPositioner.hpp>
 
-const SP<CTexture> tex = ImgUtils::load("/home/zac/stone-border.png");
-const SP<CTexture> tl_tex = ImgUtils::sliceTexture(tex, {{0, 0}, {50, 50}});
-// const SP<CTexture> tl_tex = ImgUtils::sliceTexture(tex, {{0, 0}, {70, 70}});
+const SP<CTexture> tex = ImgUtils::load("/home/zac/assets/border.png");
+const double BORDER_LEFT = 7;
+const double BORDER_RIGHT = 7;
+const double BORDER_TOP = 7;
+const double BORDER_BOTTOM = 7;
+const SP<CTexture> tex_tl =
+    ImgUtils::sliceTexture(tex, {{0., 0.}, {BORDER_LEFT, BORDER_TOP}});
+const SP<CTexture> tex_t = ImgUtils::sliceTexture(
+    tex, {{BORDER_LEFT, 0.}, {BORDER_RIGHT, BORDER_TOP}});
+const SP<CTexture> tex_tr = ImgUtils::sliceTexture(
+    tex, {{tex->m_size.x - BORDER_RIGHT, 0.}, {BORDER_RIGHT, BORDER_TOP}});
+const SP<CTexture> tex_r = ImgUtils::sliceTexture(
+    tex, {{BORDER_RIGHT, BORDER_TOP}, {tex->m_size.x, BORDER_BOTTOM}});
+const SP<CTexture> tex_br = ImgUtils::sliceTexture(
+    tex, {{tex->m_size.x - BORDER_RIGHT, tex->m_size.y - BORDER_BOTTOM},
+          {BORDER_RIGHT, BORDER_BOTTOM}});
+const SP<CTexture> tex_b = ImgUtils::sliceTexture(
+    tex, {{BORDER_LEFT, BORDER_BOTTOM}, {BORDER_RIGHT, tex->m_size.y}});
+const SP<CTexture> tex_bl = ImgUtils::sliceTexture(
+    tex, {{0., tex->m_size.y - BORDER_BOTTOM}, {BORDER_LEFT, BORDER_BOTTOM}});
+const SP<CTexture> tex_l = ImgUtils::sliceTexture(
+    tex, {{0., BORDER_TOP}, {BORDER_LEFT, BORDER_BOTTOM}});
+const auto TEX_SCALE = 5.;
 
 CImgBorder::CImgBorder(PHLWINDOW pWindow) : IHyprWindowDecoration(pWindow) {
   m_pWindow = pWindow;
@@ -29,8 +49,8 @@ SDecorationPositioningInfo CImgBorder::getPositioningInfo() {
                DECORATION_EDGE_TOP | DECORATION_EDGE_BOTTOM;
   info.priority = 9990;
   info.desiredExtents = {
-      .topLeft = {70, 70},
-      .bottomRight = {70, 70},
+      .topLeft = {12, 12},
+      .bottomRight = {12, 12},
   };
   info.reserved = true;
   return info;
@@ -81,7 +101,50 @@ void CImgBorder::drawPass(PHLMONITOR pMonitor, const float &a) {
   // Render the textures
   // ------------
 
-  g_pHyprOpenGL->renderTexture(tl_tex, box, a);
+  // Save previous values
+
+  const auto wasUsingNearestNeighbour =
+      g_pHyprOpenGL->m_renderData.useNearestNeighbor;
+  const auto prevUVTL = g_pHyprOpenGL->m_renderData.primarySurfaceUVTopLeft;
+  const auto prevUVBR = g_pHyprOpenGL->m_renderData.primarySurfaceUVBottomRight;
+
+  // Do the thing
+
+  g_pHyprOpenGL->m_renderData.useNearestNeighbor = true;
+
+  g_pHyprOpenGL->m_renderData.primarySurfaceUVTopLeft = {0, 0};
+  // g_pHyprOpenGL->m_renderData.primarySurfaceUVBottomRight = {
+  //     box.width / tex_tl->m_size.x / TEX_SCALE,
+  //     box.height / tex_tl->m_size.y / TEX_SCALE};
+  g_pHyprOpenGL->m_renderData.primarySurfaceUVBottomRight = {1, 1};
+  const CBox box_tl = {box.pos(), tex_tl->m_size * TEX_SCALE};
+  g_pHyprOpenGL->renderTexture(tex_tl, box_tl, a, 0, 2.F, false, false,
+                               GL_REPEAT, GL_REPEAT);
+
+  const CBox box_tr = {
+      {box.x + box.width - tex_tr->m_size.x * TEX_SCALE, box.y},
+      tex_tr->m_size * TEX_SCALE};
+  g_pHyprOpenGL->renderTexture(tex_tr, box_tr, a, 0, 2.F, false, false,
+                               GL_REPEAT, GL_REPEAT);
+
+  const CBox box_br = {{box.x + box.width - tex_br->m_size.x * TEX_SCALE,
+                        box.y + box.height - tex_br->m_size.y * TEX_SCALE},
+                       tex_br->m_size * TEX_SCALE};
+  g_pHyprOpenGL->renderTexture(tex_br, box_br, a, 0, 2.F, false, false,
+                               GL_REPEAT, GL_REPEAT);
+
+  const CBox box_bl = {
+      {box.x, box.y + box.height - tex_bl->m_size.y * TEX_SCALE},
+      tex_bl->m_size * TEX_SCALE};
+  g_pHyprOpenGL->renderTexture(tex_bl, box_bl, a, 0, 2.F, false, false,
+                               GL_REPEAT, GL_REPEAT);
+
+  // Restore previous values
+
+  g_pHyprOpenGL->m_renderData.useNearestNeighbor = wasUsingNearestNeighbour;
+
+  g_pHyprOpenGL->m_renderData.primarySurfaceUVTopLeft = prevUVTL;
+  g_pHyprOpenGL->m_renderData.primarySurfaceUVBottomRight = prevUVBR;
 }
 
 eDecorationType CImgBorder::getDecorationType() { return DECORATION_CUSTOM; }
